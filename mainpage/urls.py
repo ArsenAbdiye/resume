aca59b6f1b97e03e15e5507b033424b51cf9cd62
ArsenAbdiye <arsenabdiyev@gmail.com>
2024-10-21 04:56:15 +0600

    ads

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,6 +1,12 @@
-from .views import main_banner
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+from .views import main_banner
 
 urlpatterns = [
-    path("", main_banner),
+    path("", main_banner, name='main_banner'),  # Главная страница
 ]
+
+# Добавляем поддержку медиа-файлов, если в DEBUG
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- resume/settings.py
+++ resume/settings.py
@@ -140,7 +140,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'  # URL для статических файлов
+STATICFILES_DIRS = [BASE_DIR / 'static'] 
+
+
 # CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 
 # Default primary key field type
